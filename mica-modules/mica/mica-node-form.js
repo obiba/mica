a309b7f99b2f57803831ba0e16830bb3fe613424
Cédric Thiebault <cthiebault@p3g.org>
2011-04-12 20:52:18 +0000

    Comment node reference default value in JS

--- mica-modules/mica/mica-node-form.js
+++ mica-modules/mica/mica-node-form.js
@@ -10,15 +10,14 @@ Drupal.behaviors.micaFieldsetSummaries = {
     $('fieldset.relation-node-type-settings-form', context).drupalSetSummary(function(context) {
       var vals = [];
 
-      var parentType = $(".form-item-parent-type select option:selected", context);
-      var parentValue = parentType.val();
       
-      if(parentValue) {
+      var parentType = $(".form-item-parent-type select option:selected", context);
+      if(parentType.val()) {
     	  vals.push(parentType.text());
     	  $(".form-item-node-reference-name", context).show();
-    	  if(!$(".form-item-node-reference-name input", context).val()) {
-    		  $(".form-item-node-reference-name input", context).val(parentValue + '_ref');
-    	  }
+//    	  if(!$(".form-item-node-reference-name input", context).val()) {
+//    		  $(".form-item-node-reference-name input", context).val($(".machine-name-value", context).text() + '_ref');
+//    	  }
       } else {
     	  $(".form-item-node-reference-name", context).hide();
     	  $(".form-item-node-reference-name input", context).val('');
