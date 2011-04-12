bf4850f25dc0a718686d8d6f6002d60aa9a08576
Cédric Thiebault <cthiebault@p3g.org>
2011-04-12 20:30:33 +0000

    Add relation form to content type form

---
+++ mica-modules/mica/mica-node-form.js
@@ -0,0 +1,32 @@
+(function ($) {
+
+Drupal.behaviors.micaFieldsetSummaries = {
+  attach: function (context) {
+    $('fieldset.relation-node-type-settings-form', context).drupalSetSummary(function (context) {
+      return Drupal.checkPlain($('.form-item-mica_relation input:checked', context).next('label').text());
+    });
+
+    // Provide the summary for the node type form.
+    $('fieldset.relation-node-type-settings-form', context).drupalSetSummary(function(context) {
+      var vals = [];
+
+      var parentType = $(".form-item-parent-type select option:selected", context);
+      var parentValue = parentType.val();
+      
+      if(parentValue) {
+    	  vals.push(parentType.text());
+    	  $(".form-item-node-reference-name", context).show();
+    	  if(!$(".form-item-node-reference-name input", context).val()) {
+    		  $(".form-item-node-reference-name input", context).val(parentValue + '_ref');
+    	  }
+      } else {
+    	  $(".form-item-node-reference-name", context).hide();
+    	  $(".form-item-node-reference-name input", context).val('');
+      }
+
+      return Drupal.checkPlain(vals.join(', '));
+    });
+  }
+};
+
+})(jQuery);
