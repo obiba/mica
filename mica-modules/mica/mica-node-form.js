8a0588f38747533b452b0abccae2115bc8b23623
Cédric Thiebault <cthiebault@p3g.org>
2011-04-18 20:07:23 +0000

    Replace node Mica_relation by a basic mica_relation DB table

--- mica-modules/mica/mica-node-form.js
+++ mica-modules/mica/mica-node-form.js
@@ -2,28 +2,18 @@
 
 Drupal.behaviors.micaFieldsetSummaries = {
   attach: function (context) {
+/*	  
     $('fieldset.relation-node-type-settings-form', context).drupalSetSummary(function (context) {
       return Drupal.checkPlain($('.form-item-mica_relation input:checked', context).next('label').text());
     });
-
+*/
     // Provide the summary for the node type form.
     $('fieldset.relation-node-type-settings-form', context).drupalSetSummary(function(context) {
-      var vals = [];
-
-      
       var parentType = $(".form-item-parent-type select option:selected", context);
       if(parentType.val()) {
-    	  vals.push(parentType.text());
-    	  $(".form-item-node-reference-name", context).show();
-//    	  if(!$(".form-item-node-reference-name input", context).val()) {
-//    		  $(".form-item-node-reference-name input", context).val($(".machine-name-value", context).text() + '_ref');
-//    	  }
-      } else {
-    	  $(".form-item-node-reference-name", context).hide();
-    	  $(".form-item-node-reference-name input", context).val('');
+    	  return Drupal.checkPlain(parentType.text());
       }
-
-      return Drupal.checkPlain(vals.join(', '));
+      return Drupal.checkPlain('');
     });
   }
 };
