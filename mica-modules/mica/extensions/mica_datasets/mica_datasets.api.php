b4797578f50a57f033e61e2e2b098f0d672d3fe9
Yannick Marcon <yannick.marcon@obiba.org>
2011-06-10 15:25:14 +0000

    MICA-116 Added mica dataset opal connection declaration.

--- mica-modules/mica/extensions/mica_datasets/mica_datasets.api.php
+++ mica-modules/mica/extensions/mica_datasets/mica_datasets.api.php
@@ -6,20 +6,32 @@
  */
 
 /**
- * Alter the Mica Dataset connection info.
+ * Defines one or more dataset connection classes a module offers.
  *
- * Modules may implement this hook to alter the information that defines
- * Mica Dataset connections. All properties that are available in
- * hook_mica_datasets_connection_info() can be altered here.
+ * Note: The ids should be valid PHP identifiers.
  *
- * @see hook_mica_datasets_connection_info()
- *
- * @param array $connection_info
- *   The Mica Dataset connection info array, keyed by connection id.
+ * @return array
+ *   An associative array of dataset connection classes, keyed by a unique
+ *   identifier and containing associative arrays with the following keys:
+ *   - name: The connection class' translated name.
+ *   - description: A translated string to be shown to administrators when
+ *     selecting a connection class.
+ *   - class: The service class, which has to implement the
+ *     MicaDatasetConnectionInterface interface.
  */
-function hook_mica_datasets_connection_info(array &$connection_info) {
-  foreach ($connection_info as $id => $info) {
-    $connection_info[$id]['class'] = 'MyProxyConnectionClass';
-    $connection_info[$id]['example_original_class'] = $info['class'];
-  }
+function hook_mica_datasets_connection_info() {
+  $connections['example_some'] = array(
+    'name' => t('Some Connection'),
+    'description' => t('Connection for some data repository.'),
+    'class' => 'SomeConnectionClass',
+    // Unknown keys can later be read by the object for additional information.
+    'init args' => array('foo' => 'Foo', 'bar' => 42),
+  );
+  $connections['example_other'] = array(
+    'name' => t('Other Connection'),
+    'description' => t('Connection for another data repository.'),
+    'class' => 'OtherConnectionClass',
+  );
+
+  return $connections;
 }
