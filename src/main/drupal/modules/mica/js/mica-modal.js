623217599e55fc47aef8731e574afcac71d9711f
Yannick Marcon <yannick.marcon@obiba.org>
2011-08-26 16:31:12 +0000

    MICA-191 Modal style added.

---
+++ src/main/drupal/modules/mica/js/mica-modal.js
@@ -0,0 +1,24 @@
+/**
+* Provide the HTML to create the modal dialog.
+*/
+Drupal.theme.prototype.CToolsSampleModal = function () {
+  var html = ''
+
+  html += '<div id="ctools-modal" class="popups-box">';
+  html += '  <div class="ctools-modal-content mica-modal-content">';
+  html += '    <div class="popups-container">';
+  html += '      <div class="modal-header popups-title">';
+  html += '        <span id="modal-title" class="modal-title"></span>';
+  html += '        <span class="popups-close"><a class="close" href="#">X</a></span>';
+  html += '        <div class="clear-block"></div>';
+  html += '      </div>';
+  //html += '      <div class="modal-scroll">';
+  html += '        <div id="modal-content" class="modal-content popups-body"></div>';
+  //html += '      </div>';
+  html += '    </div>';
+  html += '  </div>';
+  html += '</div>';
+
+  return html;
+
+}
