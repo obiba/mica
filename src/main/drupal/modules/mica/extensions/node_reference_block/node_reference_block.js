e4362ff1a6a1e10bc7a704ea2b0642cf0e417afc
Cédric Thiebault <cthiebault@p3g.org>
2011-11-07 20:55:41 +0000

    Add 'Excluded related types' to exclude content type like pairing from related information.

---
+++ src/main/drupal/modules/mica/extensions/node_reference_block/node_reference_block.js
@@ -0,0 +1,27 @@
+(function($) {
+
+Drupal.behaviors.fieldDescriptionSettings = {
+		attach : function(context) {
+			
+		    // The drupalSetSummary method required for this behavior is not available
+		    // on the Blocks administration page, so we need to make sure this
+		    // behavior is processed only if drupalSetSummary is defined.
+		    if (typeof jQuery.fn.drupalSetSummary == 'undefined') {
+		      return;
+		    }
+		    
+		    $('fieldset#edit-exclude-related-type', context).drupalSetSummary(function (context) {
+		        var vals = [];
+		        $('input[type="checkbox"]:checked', context).each(function () {
+		          vals.push($.trim($(this).next('label').text()));
+		        });
+		        if (!vals.length) {
+		          vals.push(Drupal.t('No exclusions'));
+		        }
+		        return vals.join(', ');
+		      });
+			
+		}
+	};
+
+})(jQuery);
