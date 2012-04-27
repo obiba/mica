ce8f2027716fee599a3330ce44b03c6b292357ad
Etienne Morency-Bachand <emorency@p3g.org>
2012-04-27 16:15:00 +0000

    MICA-493: Added a taxonomy for the recruitment sources

---
+++ src/main/drupal/modules/mica/extensions/mica_core/mica_core.api.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Mica Datasets module.
+ */
+
+/**
+ * Defines one or more operations to import taxonomies.
+ */
+function hook_taxonomies_operations_import() {
+  $operations = array();
+  
+  return $operations;
+}
