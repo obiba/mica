66fab5e8b39a1fa4dda1a1a98f3d6cadffe4defe
Cedric Thiebault <cthiebault@p3g.org>
2012-07-13 16:29:30 -0400

    Reformat code

--- src/main/drupal/modules/mica/extensions/mica_i18n/README.txt
+++ src/main/drupal/modules/mica/extensions/mica_i18n/README.txt
@@ -9,8 +9,12 @@ To have Mica in another language than english, follow these steps:
 - Enable the module "Mica_i18n" at /admin/modules
 
 - Add a language from the page /admin/config/regional/language (for now, Mica modules support french and english
-   translations, for any other language, you will have to translate those modules in you own language)
+  translations, for any other language, you will have to translate those modules in you own language)
 
 - If needed, configure Language field position for each content type (admin/structure/types).
+
+- Go to Search API administration page (admin/config/search/search_api/index) and click Save on each indexes field list.
+  Then clear & re-index each indexes.
+  See http://jira.obiba.org/jira/browse/MICA-624
 
 - To enable translation of a content type title, edit the content type and replace the title field.
--- src/main/drupal/modules/mica/extensions/mica_relation/relations-node-form.js
+++ src/main/drupal/modules/mica/extensions/mica_relation/relations-node-form.js
@@ -1,50 +1,50 @@
 (function ($) {
 
-    Drupal.behaviors.micaFieldsetSummaries = {
-      attach: function (context) {
-        
-            var showHideRelationDetails = function() {
-                var hasParent = $(".form-item-parent-bundle select option:selected", context).val() != '';
-                var isIndexed = $("form#node-type-form input#edit-indexed").attr('checked');
-                $('.form-item-cascaded').toggle(hasParent);
-                $('.form-item-indexed').toggle(hasParent);
-                $('.form-item-child-indexes').toggle(hasParent && isIndexed);
-            };          
-        
-            showHideRelationDetails();
-        
-        // Provide the summary for the node type form.
-        $('fieldset.relation-node-type-settings-form', context).drupalSetSummary(function(context) {
-            showHideRelationDetails();
-            var vals = [];
-            var parentBundle = $(".form-item-parent-bundle select option:selected", context);
-          if (parentBundle.val()) {
-            vals.push(Drupal.checkPlain(parentBundle.text()));
-            
-            var cascaded = $("form#node-type-form input#edit-cascaded");
-            if (cascaded.attr('checked')) {
-                 vals.push(Drupal.checkPlain(cascaded.siblings("label").text()));
-              }
-            
-            var indexed = $("form#node-type-form input#edit-indexed");
-            if (indexed.attr('checked')) {
-                vals.push(Drupal.checkPlain(indexed.siblings("label").text()));
-            }
+  Drupal.behaviors.micaFieldsetSummaries = {
+    attach: function (context) {
+
+      var showHideRelationDetails = function () {
+        var hasParent = $(".form-item-parent-bundle select option:selected", context).val() != '';
+        var isIndexed = $("form#node-type-form input#edit-indexed").attr('checked');
+        $('.form-item-cascaded').toggle(hasParent);
+        $('.form-item-indexed').toggle(hasParent);
+        $('.form-item-child-indexes').toggle(hasParent && isIndexed);
+      };
+
+      showHideRelationDetails();
+
+      // Provide the summary for the node type form.
+      $('fieldset.relation-node-type-settings-form', context).drupalSetSummary(function (context) {
+        showHideRelationDetails();
+        var vals = [];
+        var parentBundle = $(".form-item-parent-bundle select option:selected", context);
+        if (parentBundle.val()) {
+          vals.push(Drupal.checkPlain(parentBundle.text()));
+
+          var cascaded = $("form#node-type-form input#edit-cascaded");
+          if (cascaded.attr('checked')) {
+            vals.push(Drupal.checkPlain(cascaded.siblings("label").text()));
           }
-          return Drupal.checkPlain(vals.join(', '));
-        });
-        
-        $("form#node-type-form input#edit-indexed").change(function() {
-            showHideRelationDetails();
-            });     
-        
-        $("form#node-type-form select#edit-parent-bundle").change(function() {
-            $("form#node-type-form input#edit-cascaded").attr('checked', false);
-            $("form#node-type-form input#edit-indexed").attr('checked', false);
-            showHideRelationDetails();
-        });     
-        
-      }
-    };
+
+          var indexed = $("form#node-type-form input#edit-indexed");
+          if (indexed.attr('checked')) {
+            vals.push(Drupal.checkPlain(indexed.siblings("label").text()));
+          }
+        }
+        return Drupal.checkPlain(vals.join(', '));
+      });
+
+      $("form#node-type-form input#edit-indexed").change(function () {
+        showHideRelationDetails();
+      });
+
+      $("form#node-type-form select#edit-parent-bundle").change(function () {
+        $("form#node-type-form input#edit-cascaded").attr('checked', false);
+        $("form#node-type-form input#edit-indexed").attr('checked', false);
+        showHideRelationDetails();
+      });
+
+    }
+  };
 
 })(jQuery);
