a7d6f81777e52f8d5c3a3e89e9d7f8d516dbe9ef
Cedric Thiebault <cthiebault@p3g.org>
2012-12-17 13:35:26 -0500

    Beautify search form

---
+++ src/main/drupal/modules/mica/extensions/mica_bootstrap_config/js/mica_bootstrap_config.search.js
@@ -0,0 +1,19 @@
+// Using the closure to map jQuery to $.
+(function ($) {
+
+  Drupal.behaviors.mica_bootstrap_config_search = {
+    attach: function (context, settings) {
+      var label = $('form#search-form .form-item-keys label');
+      var input = $('#edit-keys');
+
+      input.wrap('<div class="input-prepend" />');
+      input.before('<span class="add-on"><i class="icon-search"></i></span>');
+      input.attr('placeholder', $.trim(label.html()));
+
+      label.hide();
+
+      $('button#edit-submit').hide();
+    }
+  };
+
+}(jQuery));
