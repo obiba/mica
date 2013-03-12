050fa90bff989778e58f09234d0d6c4184170c5c
Etienne Morency-Bachand <emorency@p3g.org>
2013-03-12 12:10:12 -0400

    Adding templates for contry fields and field-design

---
+++ src/main/drupal/themes/mica_bootstrap/templates/views-view-field--variables-dimensions--page--nothing.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This template is used to print a single field in a view. It is not
+ * actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the
+ * template is perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+
+
+if (!empty($output)){
+  mica_core_prepare_tooltip();
+
+  $tooltip = trim($output);
+
+  $output = "<div id='dimension_". $row->entity->nid. "' class='tooltip'><img src='".base_path() . drupal_get_path('module', 'mica_datasets') . '/images/tick.png'."'></div>";
+  $output .= "<div id='dimension_". $row->entity->nid. "_tooltip' class='tooltip-content'>". $tooltip ."</div>";
+  $output .= "</div>";
+}
+
+?>
+
+<?php print $output; ?>
