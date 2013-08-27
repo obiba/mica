1c227050399922b058e717144ab39ddfd6ef0629
Etienne Morency-Bachand <emorency@p3g.org>
2013-08-27 12:32:02 -0400

    MICA-1150: Update with uuid when importing from Opal

--- tools.mk
+++ tools.mk
@@ -10,7 +10,7 @@ install-solr:
 	mkdir -p solr && \
 	cd solr && \
 	echo ">>> Downloading SolR (this may take a few minutes)." && \
-	wget -q http://mirror.csclub.uwaterloo.ca/apache/lucene/solr/$(solr_version)/solr-$(solr_version).tgz && \
+	wget -q http://mirror.csclub.uwaterloo.ca/apache/lucene/solr/$(solr_version)/solr-$(solr_version).tgz -o solr-$(solr_version).tgz && \
 	tar -zxf solr-$(solr_version).tgz && \
 	cp ../target/mica-dev/profiles/mica_distribution/modules/search_api_solr/solr-conf/4.x/* solr-$(solr_version)/example/solr/collection1/conf && \
 	rm solr-$(solr_version).tgz
