7d62f4bdadc46d4a9cdb8f46b5c56fa2e27ba6a5
Samir Ben Sghaier <sbensghaier@maelstrom-research.org>
2013-09-17 17:23:50 -0400

    Mica Import implementation :  Module Mica import created  Module mica Study Import created  Form upload  file Unzip in tmp/ feature done

--- src/main/drupal/modules/mica/extensions/mica_import/mica_import.api.php
+++ src/main/drupal/modules/mica/extensions/mica_import/mica_import.api.php
@@ -6,38 +6,21 @@
  */
 
 /**
- * @param $node
- * @return array of nid to import with specified node
+ * @return array permission parameter to access this features
  */
-function hook_mica_import_find_related_nids($node) {
-  $nids = array();
-  $nids[] = $node->nid;
-  $node_wrapper = entity_metadata_wrapper('node', $node->nid);
-  foreach ($node_wrapper->field_contacts_ref->getIterator() as $contact_wrapper) {
-    $nids[] = $contact_wrapper->nid->value();
-  }
-  return $nids;
+function hook_mica_import_permission_parameters() {
+  return array(
+    'import studies' => array(
+      'title' => t('Import Studies'),
+      'description' => t('Allow users importing studies')
+    )
+  );
 }
 
 /**
- * @param $node node to serialize
- * @param $temp_folder_path folder where to copy attachments
- * @return XML
+ * @return true, false to verify if user have permission to this features
  */
-function hook_mica_import_to_xml($node, $temp_folder_path) {
-  $wrapper = entity_metadata_wrapper('node', $node);
-
-  if ($node->type === 'study') {
-    $files = $wrapper->field_files->value();
-    if (isset($files)) {
-      mica_import_copy_attachment_file($files, $node->type, $temp_folder_path);
-    }
-    $dom = new DomDocument('1.0', 'utf-8');
-    $root = $dom->createElement($node->type);
-    $root->setAttribute('uuid', $wrapper->uuid->value());
-    $dom->appendChild($root);
-    return $dom->saveXML();
-  }
-
-  return NULL;
+function hook_mica_import_can_import_node() {
+  return user_access('import studies');
 }
+
