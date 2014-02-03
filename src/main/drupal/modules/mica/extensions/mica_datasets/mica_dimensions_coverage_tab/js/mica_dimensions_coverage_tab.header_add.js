edc18e134f56e3fe6b6c7b334c0feee68b94bc97
Samir Ben Sghaier <sbensghaier@maelstrom-research.org>
2014-02-03 17:00:37 -0500

    Pull request #170

--- src/main/drupal/modules/mica/extensions/mica_datasets/mica_dimensions_coverage_tab/js/mica_dimensions_coverage_tab.header_add.js
+++ src/main/drupal/modules/mica/extensions/mica_datasets/mica_dimensions_coverage_tab/js/mica_dimensions_coverage_tab.header_add.js
@@ -10,20 +10,7 @@
       });
 
       $("<tr class='study-group-header'  role='row'>" + content_headstudy + "</tr>").insertBefore("#example > thead > tr:first");
-
-      /*
-       var content_headdce = "<th colspan='2'>" + settings.dce_title + "</th>";
-       $.each(settings.header_dce_to_add, function (index, value) {
-       if (value.data) {
-       var group_color = "group_color";
-       }
-       content_headdce += "<th class='study-group-header-title " + group_color + "' colspan='" + value.colspan + "'>" + value.data + "</th>";
-       });
-
-       $("<tr class='study-group-header'  role='row'>" + content_headdce + "</tr>").insertAfter("#example > thead > tr.study-group-header:first");
-       */
-
     }
   }
-// array('class'=> $is_bootstrap ? '' : 'tooltip',  'data-original-title' =>'this is tooltip', 'data-content' =>'content tooltip')
+
 })(jQuery);
